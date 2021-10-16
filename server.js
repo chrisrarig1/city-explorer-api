6bf04299889ea54ad2b50f13d40d51b69c8c3365
Chris Rarig <cwrarig20@gmail.com>
2021-10-16 13:38:37 -0500

    added modules

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 'use strict';
 
-
 const axios = require('axios');
-//Express server
 const express = require('express');
-
-require('dotenv').config();
-//cors is security stuff
 const cors = require('cors');
 const { response, request } = require('express');
-
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 3001;
+app.use(cors());
+require('dotenv').config();
 
 app.listen(PORT, () => console.log('listenting on port', PORT));
 
@@ -20,64 +15,15 @@ app.listen(PORT, () => console.log('listenting on port', PORT));
 app.get('/', (request, response) => {
   response.send('Greetings');
 });
-//End Boilerplate
 
-app.get('/weather', handleWeather);
-app.get(`/movie`,handleMovie);
-
-async function handleWeather (request, response){
-
-  let lat = request.query.lat;
-  let lon = request.query.lon;
-  let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=3&key=${process.env.WEATHER_API_KEY}`;
-  try{
-    let weatherInfo = await axios.get(weatherUrl);
-    let cityWeather = weatherInfo.data.data;
-    let forecast = cityWeather.map(day => new Forecast(day));
-    console.log(forecast);
-    response.send(forecast);
-  }
-  catch(error){
-    console.log('cant find city');
-    response.status(404).send('City not found');
-  }
-}
-
-class Forecast {
-  constructor(day){
-    this.date = day.datetime;
-    this.description = `Low of ${day.low_temp}, high of ${day.max_temp} with ${day.weather.description}`;
-  }
-}
-
+//catch all error on root
 app.get('*', (request,response) => {
   response.status(404).send('that route does not exist');
 });
+//End Boilerplate
 
-async function handleMovie (request, response){
-  let city = request.query.searchQuery;
-  let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
-  try{
-    let getMovie = await axios.get(movieUrl);
-    let movie = getMovie.data.results;
-    let movieData = movie.map(title => new Movie(title));
-    console.log(movie);
-    response.send(movieData);
-  }
+let handleMovie = require('./modules/movie');
+let handleWeather = require('./modules/weather');
 
-  catch(error){
-    console.log('movie not found');
-    response.status(404).send('movie not found');
-  }
-}
-class Movie {
-  constructor(title){
-    this.title = title.original_title;
-    this.overview = title.overview;
-    this.average_votes = title.vote_average;
-    this.total_votes = title.vote_count;
-    this.image = 'https://www.themoviedb.org/t/p/original'+title.poster_path;
-    this.popularity = title.popularity;
-    this.release_date = title.release_date;
-  }
-}
+app.get('/weather', handleWeather);
+app.get(`/movie`,handleMovie);
